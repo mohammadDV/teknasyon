d55f6ddf732217f3e4f301e781c35f4f5135e457
mohammadDV <mohammad.daneshmand@gmail.com>
2021-08-30 16:38:01 +0300

    fix routing system

--- system/Router/Routing.php
+++ system/Router/Routing.php
@@ -25,7 +25,7 @@ class Routing {
             $this->notfound();
         }
         $classPath  = str_replace("\\","/",$match["class"]);
-        $path       = Config::get('app.BASE_DIR') . "/App/http/controllers/" . $classPath . ".php";
+        $path       = Config::get('app.BASE_DIR') . "/app/Http/Controllers/" . $classPath . ".php";
         if (!file_exists($path)){
             throw new Exp("not found class");
         }
